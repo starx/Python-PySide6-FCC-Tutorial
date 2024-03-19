eb1454bc27adb940edee6d1deb0e339b418b02f7
starx <swe_r3p0x9@mrnepal.com>
2024-03-19 09:16:55 +0000

    show hello world

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 
 # Sys module allows to process command line arguments
 import sys
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = QMainWindow()
+window.setWindowTitle("First MainWindow App!")
+
+label = QLabel()
+label.setText("Hello world")
+
+window.setCentralWidget(label)
+
+# Show the window
 window.show()
 
 # Start the event loop

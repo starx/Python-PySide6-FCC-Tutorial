c673d9dda8e5bc00f7e93a9182b2580ba2b20d1d
Starx <mail@mrnepal.com>
2024-03-19 09:12:43 +0000

    document

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication, QWidget
 
+# Sys module allows to process command line arguments
 import sys
 
 app = QApplication(sys.argv)
@@ -7,4 +8,5 @@ app = QApplication(sys.argv)
 window = QWidget()
 window.show()
 
+# Start the event loop
 app.exec()
